5e2110fd7197969898ca019f89b14d20edad9726
Adi Golos <golos.adi.03@gmail.com>
2026-08-30 13:28:30 +0200

    added working links/routes to various new html pages; separated style.css to replace internal css; created basic WIP html pages for each section in menu

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/cs-projects")
+def csprojects():
+    return render_template("csprojects.html")
+
+@app.route("/reading-writing")
+def readingwriting():
+    return render_template("readingwriting.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
